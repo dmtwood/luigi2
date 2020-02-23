12d2fa07ac8998ae8070d75976eed190694021f4
dmtG <dimitri.gevers@gmail.com>
2020-02-23 14:52:49 +0100

    untill XV. p.38

--- src/main/java/be/vdab/luigi/controllers/PizzaController.java
+++ src/main/java/be/vdab/luigi/controllers/PizzaController.java
@@ -3,10 +3,11 @@ package be.vdab.luigi.controllers;
 import be.vdab.luigi.domain.Pizza;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("pizzas")
@@ -23,9 +24,30 @@ public class PizzaController {
     };
 
     @GetMapping
+    // verwerkt GET requests naar /pizzas en werkt samen met pizzas.html
     public ModelAndView pizzas(){
                 //     html thymeleaf creates , name used to parse the data, private var with pizza strings
         return new ModelAndView("pizzas", "pizzas", pizzas);
+    }
+
+    // RequestMapping > pizzas & GetMapping > {id} >> URI template: pizzas/{id}
+    @GetMapping("{id}")
+
+
+    // verwerkt GET requests naar /pizza/1 (, 2, 3, ...) en werkt samen met pizza.html
+//    spring stopt de waarde van de path var met naam id in de URL van de request. vb pizzas/1 >> id=1
+    public ModelAndView pizza(@PathVariable long id) {
+
+        // create corresponding page "pizza"
+        ModelAndView modelAndView2 = new ModelAndView("pizza");
+
+        // if the pizza with this id is found, ad it to the pizza ThymeLeaf (TL) page
+        Arrays.stream(pizzas)
+                .filter(pizza -> pizza.getId() == id).findFirst()
+                .ifPresent(pizza -> modelAndView2.addObject( pizza));
+//                .ifPresent(modelAndView2::addObject);
+                                    // spring neemt automatisch lowercase ClassName als naam van de TL-data
+        return modelAndView2;
     }
 
 
